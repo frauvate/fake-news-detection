b4526d42d6487723a2820eab28a2994b1bfbb883
esma <146848430+frauvate@users.noreply.github.com>
2025-11-03 14:46:06 +0300

    Revert "Crawler sistemi tamamlandı - 818 haber eklendi"
    This reverts commit 2a40f78d9865b291818ac0b9dd01a9c88772554c.

--- manual_crawlers/ntv.py
+++
@@ -1,9 +0,0 @@
-# manual_crawlers/ntv.py  
-"""
-NTV Manuel Crawler (Yedek)
-RSS varken kullanmayız
-"""
-
-def crawl_ntv():
-    """NTV için manuel crawler (şimdilik kullanılmıyor)"""
-    return []
